976fc395f60a23b9b2ffd35a2de3d4150f695735
omeratagun <omratagn@gmail.com>
2018-02-02 15:52:34 +0300

    Add files via upload
    <html>
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"</script>
    <body>
    <header>
    </header>
    <center>
    
    <?php
    
    
    ?>
    </center>
    </body>
    </html>

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"</script>
+<body>
+<header>
+</header>
+<center>
+
+<?php
+
+
+?>
+</center>
+</body>
+</html>
