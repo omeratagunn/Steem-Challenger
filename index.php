5b161a97cbdd7bb94dc7920dcea85af63cebbc7e
omeratagun <omratagn@gmail.com>
2018-02-02 22:50:10 +0300

    Jealousy warns added. Working on visual issues.
    Dont be jealous, be relax.

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="js/bootstrap.min" rel="stylesheet">
 	<link href="css/bootstrap.min" rel="stylesheet">
+	<link href="style.css" rel="stylesheet">
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
 
 <body>
 <header>
@@ -23,11 +25,11 @@
     <input name="myself" class="form-control mr-sm-2" type="text" placeholder="Your username" >
 	<input name="someone" class="form-control mr-sm-1" type="text" placeholder="Whois?" >
 	<br>	
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="check">Check it out!</button>
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="check">Im so jealous</button>
   </form>
 </nav>
 <?php
-
+     
     $error = false;
     $payment = 0;
 	$payment_someone = 0;
@@ -35,11 +37,12 @@ if(isset($_POST['check'])){
 	
 	$meme = $_POST['myself'];
 	$himhim = $_POST['someone'];
-	
+	// if both inputs are empty give error //
 	if(empty($meme && $himhim)){
 	$error = true;
 	echo "Fill the both inputs";
 	}
+	// both inputs filled, then give the result //
     if($error == false){
     $someone_url = 'https://api.steemjs.com/get_discussions_by_blog?query={"tag":"'.$himhim.'","limit":"100"}';
     $myself_url = 'https://api.steemjs.com/get_discussions_by_blog?query={"tag":"'.$meme.'","limit":"100"}';
@@ -54,7 +57,8 @@ if(isset($_POST['check'])){
 	$author=$person1['author'];	
 	
 	
-	
+	$post_link_author = "https://www.steemit.com/@".$person1["author"]."/".$person1["permlink"];
+	// no need to get data of 0.00 SBD //
 	if(!($person1["pending_payout_value"] == "0.00 SBD")){
     
 	$total_amount_mine = str_replace(" SBD", "", $person1["pending_payout_value"]);
@@ -73,14 +77,77 @@ if(isset($_POST['check'])){
 	$payment_someone = $payment_someone + $total_amount_someone;
 	
 	}
-	
 	}
-	echo '<left>Author: '.$author.'</left>';
-	echo 'Total SBD:'.$payment.'';
-	echo '<right>Author: '.$author_person2.'</right>';
+	if($payment_someone > $payment){
+		
+		// if someone which means the username in second input have bigger amount of pending payment - BE JEALOUS //
+		$jealousy = "Im so jealous on you!!!!";
+		echo $jealousy;
+    }
+	    // ELSE DONT CARE ABOUT HIM //
+		else{
+			$dont_care = "I dont care, im not jealous";
+			echo $dont_care;
+		}
+	echo '<br>';
+    echo '<div class="container">
+	
+		<div class="col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+    	 <div class="well profile">
+            <div class="col-sm-12">
+                <div class="col-xs-12 col-sm-8">
+                    <h2>'.$author.'</h2>
+                </div>             
+                <div class="col-xs-12 col-sm-4 text-center">
+                </div>
+            </div>            
+            <div class="col-xs-12 divider text-center">
+                <div class="col-xs-12 col-sm-4 emphasis">
+            
+                <div class="col-xs-12 col-sm-4 emphasis">
+                    <h2><strong>'.$payment.'$</strong></h2>                    
+                    <p><small>Pending payments</small></p>
+                    <div class="btn-group dropup btn-block">
+                      </button>
+                      
+                    </div>
+                </div>
+            </div>
+    	 </div>                 
+		</div>
+	</div>
+</div>
+	     <div class="col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+    	 <div class="well profile">
+            <div class="col-sm-12">
+                <div class="col-xs-12 col-sm-8">
+                    <h2>'.$author_person2.'</h2>
+                </div>             
+                <div class="col-xs-12 col-sm-4 text-center">  
+                </div>
+            </div>            
+            <div class="col-xs-12 divider text-center">
+               
+                    <div class="col-xs-12 col-sm-4 emphasis" allign="center">
+                    <h2><strong>'.$payment_someone.'$</strong></h2>                    
+                    <p><small>Pending payments</small></p>
+                    <div class="btn-group dropup btn-block">
+                      </button>
+                      
+                    </div>
+                </div>
+            </div>
+    	 </div>                 
+		</div>
+	</div>
+</div>
+	';
+  
     }
     }
 ?>
+
+
 </center>
 </body>
 </html>
