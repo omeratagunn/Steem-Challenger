65d5d7467822e72fa3ba6313966cfe8a4c20b043
omeratagun <omratagn@gmail.com>
2018-02-02 21:11:34 +0300

    Update index.php
    From the first input, user date taken via steemit API

--- index.php
+++ index.php
@@ -16,17 +16,46 @@
 
 </header>
 <center>
+
 <img src="steemit.png" alt="Steemit" height="200" width="500">
 <nav class="navbar navbar-light bg-faded">
-  <form method="GET" class="form-inline">
-    <input class="form-control mr-sm-2" type="text" placeholder="Your username" name="user1">
-	<input class="form-control mr-sm-1" type="text" placeholder="Someone else" name="user2">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Check it out!</button>
+  <form method="POST" class="form-inline">
+    <input name="myself" class="form-control mr-sm-2" type="text" placeholder="Your username" >
+	<input name="user2" class="form-control mr-sm-1" type="text" placeholder="Someone else" >
+	<br>	
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="check">Check it out!</button>
   </form>
 </nav>
 <?php
 
+    $error = false;
+    $payment = 0;
+if(isset($_POST['check'])){
+	
+	$meme = $_POST['myself'];
+    if($error == false){
 
+    $myself_url = 'https://api.steemjs.com/get_discussions_by_blog?query={"tag":"'.$meme.'","limit":"100"}';
+    $json= file_get_contents($myself_url);
+    $data = json_decode($json,true);
+  
+    foreach ($data as $person1) {
+	try
+	{
+	
+	if(!($person1["pending_payout_value"] == "0.00 SBD")){
+    $author=$person1['author'];
+	$total_amount_mine = str_replace(" SBD", "", $person1["pending_payout_value"]);
+	$payment = $payment + $total_amount_mine;
+	}
+   
+	}
+	catch(Exception $me){}
+    }
+	echo '<left>Author: '.$author.'</left>';
+	echo 'Total SBD:'.$payment.'';
+    }
+    }
 ?>
 </center>
 </body>
