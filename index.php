0880cffd890fea1e6e8117f8784041ac30e99ae9
omeratagun <omratagn@gmail.com>
2018-02-02 16:24:47 +0300

    Update index.php
    <html>
      <meta charset="utf-8">
        <meta http-equiv="X-UA-Compatible" content="IE=edge">
        <meta name="viewport" content="width=device-width, initial-scale=1">
        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
        <title>Bootstrap 101 Template</title>
    	<link href="style.css">
    
        <!-- Bootstrap -->
        <link href="css/bootstrap.min.css" rel="stylesheet">
    	<link href="js/bootstrap.min" rel="stylesheet">
    	<link href="css/bootstrap.min" rel="stylesheet">
    
    <body>
    <header>
    
    </header>
    <center>
    <img src="steemit.png" alt="Steemit" height="200" width="500">
    <nav class="navbar navbar-light bg-faded">
      <form method="GET" class="form-inline">
        <input class="form-control mr-sm-2" type="text" placeholder="Your username" name="user1">
    	<input class="form-control mr-sm-1" type="text" placeholder="Someone else" name="user2">
        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Check it out!</button>
      </form>
    </nav>
    <?php
    
    
    ?>
    </center>
    </body>
    </html>

--- index.php
+++ index.php
@@ -1,11 +1,29 @@
 <html>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"</script>
+  <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+	<link href="style.css">
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="js/bootstrap.min" rel="stylesheet">
+	<link href="css/bootstrap.min" rel="stylesheet">
+
 <body>
 <header>
+
 </header>
 <center>
-
+<img src="steemit.png" alt="Steemit" height="200" width="500">
+<nav class="navbar navbar-light bg-faded">
+  <form method="GET" class="form-inline">
+    <input class="form-control mr-sm-2" type="text" placeholder="Your username" name="user1">
+	<input class="form-control mr-sm-1" type="text" placeholder="Someone else" name="user2">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Check it out!</button>
+  </form>
+</nav>
 <?php
 
 
