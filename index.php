01b0561c02e8efc740d772a6c75f798ffa2c1d81
omeratagun <omratagn@gmail.com>
2018-02-04 03:18:55 +0300

    System upgraded
    - From now on only 1 input
    - SBD usd price and TRY prices included the system from allcoincapmarket.
    - Results comes with calculation based on beneficaries system and gives the result both usd and try.

--- index.php
+++ index.php
@@ -21,36 +21,45 @@
 
 <img src="steemit.png" alt="Steemit" height="200" width="500">
 <nav class="navbar navbar-light bg-faded">
-  <form method="POST" class="form-inline">
-    <input name="myself" class="form-control mr-sm-2" type="text" placeholder="Your username" > vs
-	<input name="someone" class="form-control mr-sm-1" type="text" placeholder="Someone you jealous" >
+  <form method="GET" class="form-inline">
+    <input name="myself" class="form-control mr-sm-2" type="text" placeholder="Your username" >
 	<br>	
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="check">Im so jealous</button>
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="check">Give it to me</button>
   </form>
 </nav>
+	System calculates your "total pending payouts" and gives you the amount how much you earned as American Dollars and Turkish liras by the current market ratio from "https://coinmarketcap.com/coins/views/all/"<br>
+	Sistem "bekleyen ödelemerinizi" yani, bekleyen postlarınızın toplam değerini alır(içerde çarpar böler) ve Size sonucu Amerikan doları ve türk lirası olarak güncel "https://coinmarketcap.com/coins/views/all/" market değerine göre hesaplar.<br>
+	
+	<br>
+	<b>Note: Calculation based on beneficaries system(dmania, dlive, utopian posts). If you are posting only on steemit results might be different<br>
+	Not: Hesaplamalar dmania, dlive, utopian postlarının kesintilerine göre yapılmıştır. Eğer steemit postu atıyorsanız hesap farklı çıkabilir.
+	
 <?php
-     
     $error = false;
     $payment = 0;
 	$payment_someone = 0;
-if(isset($_POST['check'])){
+if(isset($_GET['check'])){
 	
-	$meme = $_POST['myself'];
-	$himhim = $_POST['someone'];
+	$meme = $_GET['myself'];
 	// if both inputs are empty give error //
-	if(empty($meme && $himhim)){
+	if(empty($meme)){
 	$error = true;
 	echo "Fill the both inputs";
 	}
 	// both inputs filled, then give the result //
     if($error == false){
-    $someone_url = 'https://api.steemjs.com/get_discussions_by_blog?query={"tag":"'.$himhim.'","limit":"100"}';
     $myself_url = 'https://api.steemjs.com/get_discussions_by_blog?query={"tag":"'.$meme.'","limit":"100"}';
-    $json= file_get_contents($myself_url);
-	$json_someone= file_get_contents($someone_url);
-	$data_someone= json_decode($json_someone,true);
+	$json= file_get_contents($myself_url);
     $data = json_decode($json,true);
-  
+	// GET data from https://coinmarketcap.com/coins/views/all/ //
+	$sbdvalue = 'https://api.coinmarketcap.com/v1/ticker/steem-dollars/?convert=TRY';
+	$jsonsbd = file_get_contents($sbdvalue);
+	$data_sbd = json_decode($jsonsbd, true);
+	// get usd and TRY prices// 
+	$dollarprice = $data_sbd[0]['price_usd'];
+	$tryprice = $data_sbd[0]['price_try'];
+	
+	// Create the loop //
     foreach ($data as $person1) {
 	try
 	{
@@ -63,65 +72,40 @@ if(isset($_POST['check'])){
     
 	$total_amount_mine = str_replace(" SBD", "", $person1["pending_payout_value"]);
 	$payment = $payment + $total_amount_mine;
+	$paymentsbd_mine = $payment * 75 / 100;
+	$paymentsbd_mine2 = $paymentsbd_mine * 75 / 100;
+	$payment_end_mine = $paymentsbd_mine2 / 2;
+	$dollar_mine_total = $payment_end_mine * $dollarprice;
+	$try_mine_total = $payment_end_mine * $tryprice;
+	
 	}
    
 	}
 	catch(Exception $me){}
     }
-	foreach ($data_someone as $person2){
-	
-	
-	$author_person2 = $person2['author'];
-	if(!($person2["pending_payout_value"] == "0.00 SBD")){
-	$total_amount_someone = str_replace(" SBD", "", $person2["pending_payout_value"]);
-	$payment_someone = $payment_someone + $total_amount_someone;
-	
-	}
-	}
-	if($payment_someone > $payment){
-		
-		// if someone which means the username in second input have bigger amount of pending payment - BE JEALOUS //
-		$jealousy = '<div class="alert alert-danger"><strong>Damn,</strong> Im so jealous on you!!!!</div>';
-		echo $jealousy;
-    }
-	    // ELSE DONT CARE ABOUT HIM //
-		else{
-			$dont_care = '<div class="alert alert-danger"><strong>Yeah!!!</strong> Its okay, im not jealous :(</div>';
-			echo $dont_care;
 		}
-	echo '<br>';
-    echo '<div class="float-left">
-	
-		<div class="col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
-    	 <div class="well profile">
-            <div class="col-sm-12">
-                <div class="col-xs-12 col-sm-8">
-                    <h2>'.$author.' vs '.$author_person2.' | Pending payouts</h2>
-                </div>             
-                <div class="col-xs-12 col-sm-4 text-center">
-                </div>
-            </div>            
-            <div class="col-xs-12 divider text-center">
-                <div class="col-xs-12 col-sm-4 emphasis">
-            
-                  <div class="col-xs-12 col-sm-4 text-center">
-                    <h2>'.$author.' '.$payment.'$</h2><br>
-                    <h2><strong>'.$author_person2.' '.$payment_someone.'$</h2>                 
-                    <div class="btn-group dropup btn-block">
-                      </button>
-                      
-                    </div>
-                </div>
-            </div>
-    	 </div>                 
-		</div>
-	</div>
-</div>
-	     
-	';
+	echo '<table class="table">
+   <thead class="thead-inverse">
+    <tr>
+      <th>#</th>
+      <th>Username</th>
+      <th>American dollars</th>
+      <th>Turkish liras</th>
+    </tr>
+   <thead class="thead-inverse">
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>'.$author.'</td>
+      <td>'.$dollar_mine_total.' $</td>
+      <td>'.$try_mine_total.' TRY</td>
+    </tr>
+
+  </tbody>
+</table>';
   
     }
-    }
+   
 ?>
 
 
