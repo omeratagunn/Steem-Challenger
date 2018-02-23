0bb6b11039425b5f75f257b381c1824bd50e14fb
Diego Hernandes <diego@hernandev.com>
2018-02-23 17:32:02 -0300

    home page component, structure and routes

---
+++ src/app/home/routes.js
@@ -0,0 +1,14 @@
+/**
+ * Movements module routes.
+ */
+
+// import components
+import Home from './home/home.vue'
+
+const routes = [
+  { name: 'home', component: Home, path: '/' }
+]
+
+export default [
+  ...routes
+]
