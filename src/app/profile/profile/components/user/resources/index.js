ebd404e333735904bb07ba2fc758f3fd7b2993f2
Diego Hernandes <diego@hernandev.com>
2018-02-23 17:34:26 -0300

    profile user information box (profile info)

---
+++ src/app/profile/profile/components/user/resources/index.js
@@ -0,0 +1,11 @@
+import components from './components'
+import computed from './computed'
+import filters from './filters'
+import props from './props'
+
+export default {
+  components,
+  computed,
+  filters,
+  props
+}
---
+++ src/app/profile/profile/components/user/user.vue
@@ -0,0 +1,13 @@
+<script>
+// import resources mixin.
+import resources from './resources'
+
+export default {
+  name: 'ch-profile-user',
+  mixins: [ resources ]
+}
+</script>
+
+<template lang="pug" src="./user.pug"></template>
+
+<style lang="scss" src="./user.scss"></style>
