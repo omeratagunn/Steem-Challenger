d5c8e3c89b4697d02d17e8be08c8af56be86c677
Diego Hernandes <diego@hernandev.com>
2018-02-23 17:34:57 -0300

    profile page wrapper

---
+++ src/app/profile/profile/profile.vue
@@ -0,0 +1,13 @@
+<script>
+// import resources mixin.
+import resources from './resources'
+
+export default {
+  name: 'ch-profile-view',
+  mixins: [ resources ]
+}
+</script>
+
+<template lang="pug" src="./profile.pug"></template>
+
+<style lang="scss" src="./profile.scss"></style>
