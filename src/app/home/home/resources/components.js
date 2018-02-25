6bdf92ef9728625fbd0df21e044c489146e57f15
Diego Hernandes <diego@hernandev.com>
2018-02-25 16:57:46 -0300

    update home component with user search box

--- src/app/home/home/resources/components.js
+++ src/app/home/home/resources/components.js
@@ -1,9 +1,10 @@
 // imports.
 import chBox from 'src/app/layout/parts/box/box'
-
+import chUserSearch from '@/app/home/home/components/user-search/user-search'
 /**
  * Child component exports.
  */
 export default {
-  chBox
+  chBox,
+  chUserSearch
 }
--- src/app/home/home/resources/methods.js
+++ src/app/home/home/resources/methods.js
@@ -1,12 +1,28 @@
 // imports.
+import { isString } from 'lodash'
 
 /**
  * Home component methods.
  */
 export default {
-  redirect () {
-    const username = this.query
+  /**
+   * Redirect the current username to the show page.
+   */
+  redirect (value) {
+    // get username from query or event.
+    const username = (value && isString(value)) ? value : this.query
 
+    // push the route.
     this.$router.push({ name: 'profile', params: { username } })
+  },
+
+  /**
+   * Update query value from inner component.
+   *
+   * @param value
+   */
+  updateQuery (value) {
+    // update the field.
+    this.query = value
   }
 }
