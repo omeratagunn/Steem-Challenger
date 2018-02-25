27d4344c6f6401932278f0a355152e2a5ee1d94c
Diego Hernandes <diego@hernandev.com>
2018-02-25 16:57:10 -0300

    search item

---
+++ src/app/home/home/components/search-item/item.vue
@@ -0,0 +1,29 @@
+<script>
+// imports.
+import resources from './resources'
+
+/**
+ * STEEM-CHALLENGER.
+ *
+ * User search item template component.
+ */
+export default {
+  /**
+   * Component name.
+   */
+  name: 'ch-user-search-item',
+
+  /**
+   * Component mixins.
+   */
+  mixins: [ resources ]
+}
+</script>
+
+<template lang="pug" src="./item.pug">
+  // component template.
+</template>
+
+<style lang="scss" src="./item.scss">
+  // component style.
+</style>
