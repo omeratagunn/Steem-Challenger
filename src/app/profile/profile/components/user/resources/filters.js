8dad46b5da16f2c45fc60bf109d31010d52c9631
Diego Hernandes <diego@hernandev.com>
2018-02-27 04:56:55 -0300

    steem power parsing and badge

--- src/app/profile/profile/components/user/resources/filters.js
+++ src/app/profile/profile/components/user/resources/filters.js
@@ -4,5 +4,9 @@
 export default {
   percent (value) {
     return Number(value / 10000).toLocaleString(undefined, { style: 'percent', minimumFractionDigits: 2 })
+  },
+
+  steemPower (value) {
+    return Number(value).toLocaleString(undefined, { style: 'decimal', minimumFractionDigits: 0, maximumFractionDigits: 3 })
   }
 }
