a58a1f8ebf3761ed5af61698b76bb9cda6266c91
Diego Hernandes <diego@hernandev.com>
2018-03-15 20:08:47 -0300

    delegation improvements and layout

--- src/app/profile/profile/components/user/resources/index.js
+++ src/app/profile/profile/components/user/resources/index.js
@@ -1,11 +1,13 @@
 import components from './components'
 import computed from './computed'
 import filters from './filters'
+import methods from './methods'
 import props from './props'
 
 export default {
   components,
   computed,
   filters,
+  methods,
   props
 }
