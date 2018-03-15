d2e9ed7f3954ed16a8b91eadbf6b048425f4396f
Diego Hernandes <diego@hernandev.com>
2018-03-15 01:30:51 -0300

    user ranking and layout improvements

--- src/app/profile/profile/components/user/resources/computed.js
+++ src/app/profile/profile/components/user/resources/computed.js
@@ -1,3 +1,4 @@
+import { get } from 'lodash'
 import { mapGetters } from 'vuex'
 
 export default {
@@ -10,5 +11,45 @@ export default {
     'website',
     'location',
     'profileImage'
-  ])
+  ]),
+
+  /**
+   * Calculate the amount of stars.
+   */
+  vests () {
+    const ownVests = parseFloat(get(this.account, 'vesting_shares', '0 VESTS').replace(' VESTS', ''))
+    // const receivedVests = toNumber(get(this.account, 'received_vesting_shares', '0 VESTS').replace(' VESTS', ''))
+    // const delegatedVests = toNumber(get(this.account, 'delegated_vesting_shares', '0 VESTS').replace(' VESTS', ''))
+
+    // return Math.floor(ownVests + receivedVests + delegatedVests)
+    return Math.floor(ownVests)
+  },
+
+  rank () {
+    // get the vests number.
+    const vests = this.vests
+
+    // under a million vests.
+    if (vests < (10 * 10 * 10 * 1000)) {
+      return 'Plankton'
+    }
+
+    // under 10 million vests.
+    if (vests < (10 * 10 * 10 * 10 * 1000)) {
+      return 'Minnow'
+    }
+
+    // under 100 million vests.
+    if (vests < (10 * 10 * 10 * 10 * 10 * 1000)) {
+      return 'Dolphin'
+    }
+
+    // under 1 billion vests:
+    if (vests < (10 * 10 * 10 * 10 * 10 * 10 * 1000)) {
+      return 'Orca'
+    }
+
+    // anything above that is a whale.
+    return 'Whale'
+  }
 }
